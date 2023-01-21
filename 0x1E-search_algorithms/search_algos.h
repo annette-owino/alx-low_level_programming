619ceafb5f6f113b942a61a8d9d149c6e5212672
Annette Owino <annetteawuor72@gmail.com>
2023-01-21 09:28:35 -0800

    all files

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,21 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+
+/* standard functions */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/* function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+
+
+#endif /* SEARCH_ALGOS_H */
+
